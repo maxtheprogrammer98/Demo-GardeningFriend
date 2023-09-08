514057bfa043ebcdf6cd3efb2ac2f499b9568413
Maximiliano <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-09-08 18:17:01 -0300

    Se empieza a crear nueva funcion para iniciar act cultivos

--- app/src/main/java/com/example/gardeningfrienddemo/Recomendaciones.java
+++ app/src/main/java/com/example/gardeningfrienddemo/Recomendaciones.java
@@ -2,7 +2,11 @@ package com.example.gardeningfrienddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Recomendaciones extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class Recomendaciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recomendaciones);
+        // identificando boton
+        Button buscarBtn = findViewById(R.id.buscar_btn);
     }
+
+    public void abrirActCultivos(View view){
+        //identificando text inputs
+        EditText inputTemp = findViewById(R.id.input_temperatura);
+        EditText inputEst = findViewById(R.id.input_estacion);
+        EditText inputReg = findViewById(R.id.input_region);
+        // inicializando nueva actividad
+        Intent intent = new Intent(this, Cultivos.class);
+        // TODO: AGREGAR propiedades putExtra para pasar actividaes a la nueva act.
+        startActivity(intent);
+    }
+
+
 }
