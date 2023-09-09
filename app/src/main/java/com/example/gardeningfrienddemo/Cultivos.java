a558ef5149eeb35e57a195402ef197b443220add
Maximiliano <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-09-09 17:27:33 -0300

    Se modifica layout y funcion recomendaciones
    Se cambian los EditText por ButtonRadius.

--- app/src/main/java/com/example/gardeningfrienddemo/Cultivos.java
+++ app/src/main/java/com/example/gardeningfrienddemo/Cultivos.java
@@ -2,7 +2,12 @@ package com.example.gardeningfrienddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Cultivos extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class Cultivos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cultivos);
+
+        // se reciben los valores que se pasaron via 'putExtra':
+        String valorTemperatura = getIntent().getStringExtra("valTemperatura");
+        String valorEstacion = getIntent().getStringExtra("valEstacion");
+        String valorRegion = getIntent().getStringExtra("valRegion");
+
+        // se identifican los elementos de la interfaz a modificar:
+        TextView temperatura = findViewById(R.id.temperatura_selec);
+        TextView estacion = findViewById(R.id.estacion_selec);
+        TextView region = findViewById(R.id.region_selec);
+
+        // se agregan los valores ingresados en la act anterior:
+        temperatura.append(valorTemperatura);
+        estacion.append(valorEstacion);
+        region.append(valorRegion);
+    }
+
+    public void volverReco(View view){
+        Intent intent = new Intent(this, Recomendaciones.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/gardeningfrienddemo/Recomendaciones.java
+++ app/src/main/java/com/example/gardeningfrienddemo/Recomendaciones.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.Serializable;
 
 public class Recomendaciones extends AppCompatActivity {
+
+    // 1 - contiene el id del btn seleccionado
+    String tempSelec;
+    String estSelec;
+    String regSelec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +28,97 @@ public class Recomendaciones extends AppCompatActivity {
         Button buscarBtn = findViewById(R.id.buscar_btn);
     }
 
+    public void btnTempClicked(View view){
+        // var elem selec
+        RadioButton btnSelec;
+        // se identifica que btn fue seleccionado
+        if (view.getId() == R.id.rango0) {
+            btnSelec = findViewById(R.id.rango0);
+            tempSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.rango1){
+            btnSelec = findViewById(R.id.rango1);
+            tempSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.rango2){
+            btnSelec = findViewById(R.id.rango2);
+            tempSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.rango3){
+            btnSelec = findViewById(R.id.rango3);
+            tempSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.rango4){
+            btnSelec = findViewById(R.id.rango3);
+            tempSelec = btnSelec.getText().toString();
+
+        } else {
+            tempSelec = "default";
+        }
+
+    }
+
+    public void btnEstacClicked(View view){
+        // var elem selec
+        RadioButton btnSelec;
+        // se identifica que btn fue seleccionado
+        if(view.getId() == R.id.opc_verano){
+            btnSelec = findViewById(R.id.opc_verano);
+            estSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.opc_otono){
+            btnSelec = findViewById(R.id.opc_otono);
+            estSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.opc_invierno){
+            btnSelec = findViewById(R.id.opc_invierno);
+            estSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.opc_primavera){
+            btnSelec = findViewById(R.id.opc_primavera);
+            estSelec = btnSelec.getText().toString();
+
+        } else {
+            estSelec = "default";
+        }
+
+    }
+
+    public void btnRegClicked(View view){
+        // var elem selec
+        RadioButton btnSelec;
+        // se identifica que btn fue seleccionado
+        if (view.getId() == R.id.reg_norte){
+            btnSelec = findViewById(R.id.reg_norte);
+            regSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.reg_centro){
+            btnSelec = findViewById(R.id.reg_centro);
+            regSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.reg_cuyo){
+            btnSelec = findViewById(R.id.reg_cuyo);
+            regSelec = btnSelec.getText().toString();
+
+        } else if (view.getId() == R.id.reg_patagonica){
+            btnSelec = findViewById(R.id.reg_patagonica);
+            regSelec = btnSelec.getText().toString();
+
+        } else {
+            regSelec = "default";
+        }
+    }
+
     public void abrirActCultivos(View view){
-        //identificando text inputs
-        EditText inputTemp = findViewById(R.id.input_temperatura);
-        EditText inputEst = findViewById(R.id.input_estacion);
-        EditText inputReg = findViewById(R.id.input_region);
-        // inicializando nueva actividad
+        // 1 - se crea intent
         Intent intent = new Intent(this, Cultivos.class);
-        // TODO: AGREGAR propiedades putExtra para pasar actividaes a la nueva act.
+
+        // 2 - se pasan los val necesarios via put extra
+        intent.putExtra("valTemperatura", tempSelec);
+        intent.putExtra("valEstacion", estSelec);
+        intent.putExtra("valRegion", regSelec);
+
+        // 3 - se inicializa intent
         startActivity(intent);
     }
 
