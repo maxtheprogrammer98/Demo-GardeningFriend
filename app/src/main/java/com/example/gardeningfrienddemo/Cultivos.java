1e1efedd9cb23d00294f872ba8452a81edc7ffbb
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2023-09-11 20:23:35 -0300

    Se corrige bug en cultivos

--- app/src/main/java/com/example/gardeningfrienddemo/Cultivos.java
+++ app/src/main/java/com/example/gardeningfrienddemo/Cultivos.java
@@ -1,5 +1,6 @@
 package com.example.gardeningfrienddemo;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -53,6 +55,9 @@ public class Cultivos extends AppCompatActivity {
     }
 
     private void addModelsCultivos(){
+        // flag que se activa si hay resultados que coinciden con la busqueda
+        boolean resultados = false;
+
         // 1 - se extraen todos los recursos para instanciar los cultivos
         String[] cultivosNombre = getResources().getStringArray(R.array.cultivos_nombres);
         String[] cultivosTemp = getResources().getStringArray(R.array.culvtivos_temp);
@@ -62,10 +67,17 @@ public class Cultivos extends AppCompatActivity {
         // 2 - se iteran los arrays con la informacion y se generan nuevas instancias
         for (int i = 0; i < cultivosNombre.length; i++) {
             // se valida que coincida con los parametros del usuario
-            if(cultivosTemp[i] == valorTemperatura && cultivosEst[i] == valorEstacion && cultivosReg[i] == valorRegion){
+            if(cultivosTemp[i].toString().equals(valorTemperatura) && cultivosEst[i].toString().equals(valorEstacion) && cultivosReg[i].toString().equals(valorRegion)){
                 // si es correcto se crean las instancias correspondientes
                 modelsCultivos.add(new cultivosModels(cultivosNombre[i], cultivosTemp[i], cultivosEst[i], cultivosReg[i], imagenesCultivos[i]));
+                resultados = true;
             }
+        }
+
+        // si ningun resultado coincide con la busqueda
+        if(!resultados){
+            //mensaje de error
+            Toast.makeText(this, "ningun cultivo coincide con los parametros seleccionados, lo sentimos", Toast.LENGTH_SHORT).show();
         }
 
     }
